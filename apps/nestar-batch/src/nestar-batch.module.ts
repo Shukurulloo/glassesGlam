4c8338f4ed7c35e09f338838510d0f5eefac44d6
mac <mac@macui-MacBookPro.local>
2024-05-08 16:55:41 +0900

    feat: configure env variables

--- apps/nestar-batch/src/nestar-batch.module.ts
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()], // forRoot bu .env ni o'qib beradi
   controllers: [NestarBatchController],
   providers: [NestarBatchService],
 })
