c823937a13717a4984c4787eccbe529d260a74cd
mac <mac@macui-MacBookPro.local>
2024-05-08 18:45:22 +0900

    feat: build project standards

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,17 +5,21 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(), // forRoot bu .env variable ni o'qib beradi
 		GraphQLModule.forRoot({
-			driver: ApolloDriver,   // driver: GraphQL server adapteri
-			playgroud: true,        // yGraphQL Playground interfeysini faol qiladi yoki o'chiradi. browserda mashq qilish mumkin
-			uploads: false,        // client fayl yuklashini o'chiradi qiladi
-			autoSchemaFile: true,   //Agar true bo'lsa, GraphQL sxemasi avtomatik ravishda yaratiladi
+			driver: ApolloDriver, // driver: GraphQL server adapteri
+			playgroud: true, // yGraphQL Playground interfeysini faol qiladi yoki o'chiradi. browserda mashq qilish mumkin
+			uploads: false, // client fayl yuklashini o'chiradi qiladi
+			autoSchemaFile: true, //Agar true bo'lsa, GraphQL sxemasi avtomatik ravishda yaratiladi
 		}),
-	], 
+		ComponentsModule,
+		DatabaseModule,
+	],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
 })
--- apps/nestar-api/src/app.resolver.ts
+++ apps/nestar-api/src/app.resolver.ts
@@ -1,5 +1,8 @@
 import { Query, Resolver } from '@nestjs/graphql';
 
+// graphql ni hosl qilish uchun resolverlardan foydalanamz bu graphqlda asosiy vazifani bajaradi
+// query => get, mutation => post bularni resolver orqali qilamz
+
 @Resolver() // bu decoator oddiy javascript classni Resolverg aylantrib beradi
 export class AppResolver {
 	@Query(() => String)
