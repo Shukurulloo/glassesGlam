92b6f99983509b3f399663d9e845be7d4e0c1db4
mac <mac@macui-MacBookPro.local>
2024-05-15 16:31:47 +0900

    feat: standard integration of logging interseptor

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 // nest js expressni yutib oladi
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule); //NestFactory ni ichida express bor, createni mahsuli expressga o'xshagan instance bo'ladi
 	app.useGlobalPipes(new ValidationPipe()); // validation pipe ni global tarzda integratsia qildik
+  app.useGlobalInterceptors(new LoggingInterceptor()) // request va responselarni log qilib beradi
 	await app.listen(process.env.PORT_API ?? 3000); // appModulda configni ulab bu mantiqni bajardik
 }
 bootstrap();
