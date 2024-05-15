50cc29dea6377dfb2573d21be12815dd1590a197
mac <mac@macui-MacBookPro.local>
2024-05-15 17:40:25 +0900

    feat: develop logging via global interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,17 +1,42 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 // request va responselarni log qilib beradi
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...'); // kirish fazasi
+	private readonly logger: Logger = new Logger(); // Logger => request va responeni chop etish un kerak
 
-    const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() => console.log(`After... ${Date.now() - now}ms`)), // chiqish fazasi
-      );
-  }
+	public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		// Observable FRP ni consepsiasi, promise bn bir xil
+		const recordTime = Date.now();
+		const requestType = context.getType<GqlContextType>(); // qanday turdagi requestlar backentga kelishini aniqlaash un getType kerak
+
+		if (requestType === 'http') {
+			/* Developif needed! */
+		} else if (requestType === 'graphql') {
+			/* (1) Print Request */
+			const gqlContext = GqlExecutionContext.create(context);
+			// clientdan yuborayotgan queryni sintaksini oldik
+			this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+
+			/* (2) Error handling via graphQL */
+
+			/* (3) No Errors, giving Response below */
+			return next.handle().pipe(
+				// handling qilsh jarayonida login ishga tushadi
+				tap((context) => {
+					/** responseTime: serverimz kirib kegan requstni javob berayotgan vaqtdan 
+                        serverga kirib kelgan vaqtni ayrimasiga teng bo'ladi */
+					const responseTime = Date.now() - recordTime;
+					this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, 'RESPONSE'); //logger orqali printout qilamz
+				}),
+			);
+		}
+	}
+
+	private stringify(context: ExecutionContext): string {
+		// krib kelayotgan  context object bo'ladi
+		return JSON.stringify(context).slice(0, 75); // stringni 75 gacha miqdorini olamz olamz
+	}
 }
