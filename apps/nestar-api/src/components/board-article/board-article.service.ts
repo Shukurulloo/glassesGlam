2bec0f86907199bf347164e0f3cd773e529140c2
mac <mac@macui-MacBookPro.local>
2024-05-29 18:37:01 +0900

    feat: develop likeTargetBoardArticle GraphQL api

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -15,6 +15,9 @@ import { BoardArticleStatus } from '../../libs/enums/board-article.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 @Injectable()
 export class BoardArticleService {
@@ -22,6 +25,7 @@ export class BoardArticleService {
 		@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
 		private readonly memberService: MemberService,
 		private readonly viewService: ViewService,
+		private likeService: LikeService,
 	) {}
 
 	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
@@ -63,8 +67,8 @@ export class BoardArticleService {
 			}
 
 			//meLiked
-		} // targetBoardArticle ni  memberDatasini hosl qildik.   
-		targetBoardArticle.memberData = await this.memberService.getMember(null, targetBoardArticle.memberId);// null: memberni chaqirsak uni viewni oshirib qo'ymasligi un yani uni pagesiga bormasdan turip view +1 qilmasligi un
+		} // targetBoardArticle ni  memberDatasini hosl qildik.
+		targetBoardArticle.memberData = await this.memberService.getMember(null, targetBoardArticle.memberId); // null: memberni chaqirsak uni viewni oshirib qo'ymasligi un yani uni pagesiga bormasdan turip view +1 qilmasligi un
 
 		return targetBoardArticle;
 	}
@@ -94,11 +98,12 @@ export class BoardArticleService {
 	public async getBoardArticles(memberId: ObjectId, input: BoardArticlesInquiry): Promise<BoardArticles> {
 		const { articleCategory, text } = input.search;
 		const match: T = { articleStatus: BoardArticleStatus.ACTIVE }; // faqta activelarni ko'rsatadi
-		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };// desending
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC }; // desending
 
 		if (articleCategory) match.articleCategory = articleCategory;
 		if (text) match.articleTitle = { $regex: new RegExp(text, 'i') };
-		if (input.search?.memberId) { // ayni bir memberni articlelarini ko'rmoqchi bo'lsak
+		if (input.search?.memberId) {
+			// ayni bir memberni articlelarini ko'rmoqchi bo'lsak
 			match.memberId = shapeIntoMongoObjectId(input.search.memberId);
 		}
 		console.log('match:', match);
@@ -108,7 +113,8 @@ export class BoardArticleService {
 				{ $match: match },
 				{ $sort: sort },
 				{
-					$facet: { // 2xil pipeLine tizimi
+					$facet: {
+						// 2xil pipeLine tizimi
 						list: [
 							{ $skip: (input.page - 1) * input.limit },
 							{ $limit: input.limit },
@@ -127,6 +133,29 @@ export class BoardArticleService {
 		return result[0];
 	}
 
+	public async likeTargetBoardArticle(memberId: ObjectId, likeRefId: ObjectId): Promise<BoardArticle> {
+		const target: BoardArticle = await this.boardArticleModel
+			.findOne({ _id: likeRefId, articleStatus: BoardArticleStatus.ACTIVE })
+			.exec();
+		if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		const input: LikeInput = {
+			memberId: memberId,
+			likeRefId: likeRefId,
+			likeGroup: LikeGroup.ARTICLE, // articlelarga like
+		};
+
+		const modifier: number = await this.likeService.toggleLike(input); //likeService
+		const result = await this.boardArticleStatsEditor({
+			_id: likeRefId,
+			targetKey: 'articleLikes',
+			modifier: modifier,
+		}); // memberni static datasi yangilanadi
+
+		if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+		return result;
+	}
+
 	/**  ADMIN  **/
 	public async getAllBoardArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles> {
 		const { articleStatus, articleCategory } = input.search;
@@ -141,7 +170,8 @@ export class BoardArticleService {
 				{ $match: match },
 				{ $sort: sort },
 				{
-					$facet: { //aggregation sistemni ikkiga bo'lyapmiz
+					$facet: {
+						//aggregation sistemni ikkiga bo'lyapmiz
 						list: [
 							{ $skip: (input.page - 1) * input.limit },
 							{ $limit: input.limit },
@@ -153,7 +183,7 @@ export class BoardArticleService {
 				},
 			])
 			.exec();
-				// length mavjud bo'lmasa 
+		// length mavjud bo'lmasa
 		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
 		return result[0];
@@ -168,7 +198,7 @@ export class BoardArticleService {
 			.findByIdAndUpdate({ _id: _id, articleStatus: BoardArticleStatus.ACTIVE }, input, { new: true })
 			.exec();
 
-		console.log('result:', result); 
+		console.log('result:', result);
 
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
