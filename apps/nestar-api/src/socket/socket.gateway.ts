db3dfbfc3b564f987be69ad4156882c27d499d54
mac <mac@macui-MacBookPro.local>
2024-07-01 06:24:28 +0900

    fix: modify WebSocket Chatting Server final stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -2,17 +2,23 @@ import { Logger } from '@nestjs/common';
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'ws';
 import * as WebSocket from 'ws';
+import { AuthService } from '../components/auth/auth.service';
+import { Member } from '../libs/dto/member/member';
+import * as url from 'url';
 
 interface MessagePayload {
 	// fronenda data yuborish yoki qabul qilish un
 	event: string;
 	text: string;
+	memberData: Member;
 }
 
 // kimdir serverga ulansa yangi conectionni biz uchun serverimz boshqa memberlarga data yuborishi un
 interface InfoPayload {
 	event: string;
 	totalClient: number; // yangilangan clientlar sonini yuborish uchun
+	memberData: Member;
+	action: string; // joined, left
 }
 
 // TCP connectionimz aynan qaysi transportsni qo'llashini belgilaymz. secure bo'lmasin
@@ -20,6 +26,10 @@ interface InfoPayload {
 export class SocketGateway implements OnGatewayInit {
 	private logger: Logger = new Logger('SocketEventsGateway'); //loggerlar un hizmat qiladigon state hosl qilamz nomi "SocketEventsGateway"
 	private summaryClient: number = 0; // ulangan clientlar soni
+	private clientsAuthMap = new Map<WebSocket, Member>();
+	private messagesList: MessagePayload[] = [];
+
+	constructor(private authService: AuthService) {}
 
 	@WebSocketServer()
 	server: Server;
@@ -28,39 +38,72 @@ export class SocketGateway implements OnGatewayInit {
 		// bu SocketGateway tayyor bo'gan vaqtda ishga tushadi yani logger orqali terminalda printout bo'ladi
 		this.logger.verbose(`WebSocket Server Initialized & total [${this.summaryClient}]`); // inishelayz bo'gani haqida malumot va ulangan clientlar soni
 	}
+	// kim murojat etganini aniqlaymz
+	private async retrieveAuth(req: any): Promise<Member> {
+		try {
+			const parseUrl = url.parse(req.url, true);
+			const { token } = parseUrl.query; // url core packagesi orqali tokenni olamz
+			// console.log('token:', token);
+			return await this.authService.verifyToken(token as string);
+		} catch (err) {
+			return null;
+		}
+	}
+
 	/** bu method clientlar bizni wepsocket serverimzga ulangan vaqti ishga tushadi
       1-serverga ulangan client datasi, 2-dok bo'yicha */
-	handleConnection(client: WebSocket, ...args: any[]) {
+	public async handleConnection(client: WebSocket, req: any) {
+		const authMember = await this.retrieveAuth(req); // auth bo'lgan member mavjud bo'lsa uni qiymatini qaytarsin
 		this.summaryClient++; // birinchi ulangan vaqti clientlar soni 1+ ga oshadi
-		this.logger.verbose(`Connection & total [${this.summaryClient}]`); // va terminalda shu datalarni log methodi orqali chop etamz
+		this.clientsAuthMap.set(client, authMember); // key: client => authMember qilib saqlaymz
+		// console.log('authMember:', authMember);
+
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+		this.logger.verbose(`Connection [${clientNick}] & total [${this.summaryClient}]`); // va terminalda shu datalarni log methodi orqali chop etamz
 
 		// websocket serverimzga yangi client qoshilganida umumiy clientlar sonini hamma ulangan clientlarga yunosih mantig'i
 		const infoMsg: InfoPayload = {
 			event: 'info',
 			totalClient: this.summaryClient,
+			memberData: authMember,
+			action: 'joined',
 		};
-		this.emitMessage(infoMsg);
+		this.emitMessage(infoMsg); // serverga ulangan hammaga yuborish
+		// CLIENT MESSAGES . ulangan clientni o'ziga habar yuborish mantig'i
+		client.send(JSON.stringify({ event: 'getMessages', list: this.messagesList }));
 	}
 	/** bu method wepsocket serverimzga ulangan clientlar browserdan chiqib ketganda ishga tushadi */
-	handleDisconnect(client: WebSocket) {
+	public handleDisconnect(client: WebSocket) {
+		const authMember = this.clientsAuthMap.get(client); // left bolgan memberni olib beradi
 		this.summaryClient--;
-		this.logger.verbose(`Disconnection & total [${this.summaryClient}]`);
+		this.clientsAuthMap.delete(client); // o'chiramz
+
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+		this.logger.verbose(`Disconnection [${clientNick}] & total [${this.summaryClient}]`);
 
 		const infoMsg: InfoPayload = {
 			event: 'info',
 			totalClient: this.summaryClient,
+			memberData: authMember,
+			action: 'left',
 		};
 
-		// client - disconnect
+		// client - disconnect.  uzilgandan boshqa hammaga yuborish
 		this.broadcastMessage(client, infoMsg);
 	}
 
 	@SubscribeMessage('message') // messagega subscribe bolganda
 	public async handleMessage(client: WebSocket, payload: string): Promise<void> {
-		const newMessage: MessagePayload = { event: 'message', text: payload };
+		const authMember = this.clientsAuthMap.get(client);
+		const newMessage: MessagePayload = { event: 'message', text: payload, memberData: authMember };
 
-		this.logger.verbose(`NEW MESSAGE: ${payload}`)
-		this.emitMessage(newMessage) // hamma ulangan webSocket clientlarga yuborish
+		const clientNick: string = authMember?.memberNick ?? 'Guest';
+		this.logger.verbose(`NEW MESSAGE [${clientNick}]: ${payload}`);
+
+		this.messagesList.push(newMessage);
+		if (this.messagesList.length > 5) this.messagesList.splice(0, this.messagesList.length - 5); // faqat 5ta habar
+
+		this.emitMessage(newMessage); // hamma ulangan webSocket clientlarga yuborish
 	}
 
 	// uzilgan clientdan boshqa hammasiga yuborish
@@ -81,3 +124,10 @@ export class SocketGateway implements OnGatewayInit {
 		});
 	}
 }
+
+/*
+MESSAGE TARGET: 
+	1. Client    (only client)  faqat 1taga
+	2. Broadcast (exept client) uzilgandan boshqa hammaga
+	3. Emit      (all client)   hammaga
+*/
